cfc2c9c6a3e551341398aef2de40819f96592c06
Muzmmil Khan <muzmmil.dba@gmail.com>
2026-09-13 21:57:42 +0530

    Added new API Get Book By ID

--- src/gittest/main.py
+++ src/gittest/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 
+BOOKS = [
+    {"id": 1, "title": "1984", "author": "George Orwell"},
+    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"id": 3, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"}
+]
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/books/{book_id}")
+def get_book_by_id(book_id: int):
+    for book in BOOKS:
+        if book["id"] == book_id:
+            return book
+    return {"error": "Book not found"}
