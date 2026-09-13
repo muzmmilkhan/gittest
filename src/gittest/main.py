01f0859baf4484afe0a06e6c798f2b99be6880c0
Muzmmil Khan <muzmmil.dba@gmail.com>
2026-09-14 00:45:17 +0530

    Added Endpoint for Delete, Update Book

--- src/gittest/main.py
+++ src/gittest/main.py
@@ -29,3 +29,19 @@ def get_book_by_id(book_id: int):
 def create_book(book: Book):
     BOOKS.append(book)
     return book
+
+@app.put("/books/{book_id}")
+def update_book(book_id: int, updated_book: Book):
+    for index, book in enumerate(BOOKS):
+        if book.id == book_id:
+            BOOKS[index] = updated_book
+            return updated_book
+    return {"error": "Book not found"}
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: int):
+    for index, book in enumerate(BOOKS):
+        if book.id == book_id:
+            deleted_book = BOOKS.pop(index)
+            return deleted_book
+    return {"error": "Book not found"}
